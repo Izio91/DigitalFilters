34d34b419c8677e0ac2a517ceb2860e0750d5fef
Maurizio <mau.moraca@gmail.com>
2020-10-10 22:38:58 +0200

    added MedianFilter methods

--- MedianFilter.py
+++ MedianFilter.py
@@ -1,8 +1,8 @@
 from DigitalFilter import Filter
+import statistics
 
 class Median(Filter):
     def make_convolution(self, image, output):
-        average = 1/(self.get_width() * self.get_height())
         starting_row = int(self.get_height() / 2)
         starting_column = int(self.get_width() / 2)
         ending_row = image[:, 0].size - self.get_down_side_frame()
@@ -10,9 +10,9 @@ class Median(Filter):
 
         for i in range(starting_row, ending_row):
             for j in range(starting_column, ending_column):
-                sum = 0
-                for q in range(i - self.get_up_side_frame(), i + self.get_down_side_frame()):
-                    for r in range(j - self.get_left_side_frame(), j + self.get_right_side_frame()):
-                        sum = sum + image[q, r]
-                output[i, j] = sum * average
+                tmp_list = []
+                for q in range(i - self.get_up_side_frame(), i - self.get_up_side_frame() + self.get_height()):
+                    for r in range(j - self.get_left_side_frame(), j - self.get_left_side_frame() + self.get_width()):
+                        tmp_list.append(image[q, r])
+                output[i, j] = statistics.median(tmp_list)
         return output
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import numpy as np
 from AverageFilter import Average
 from GaussianFilter import Gaussian
+from MedianFilter import Median
 
 """averageFilter = Average(3, 3)
 image = np.ones((1024, 1024))
 print(averageFilter.apply_to_image(image))"""
 
-gaussianFilter = Gaussian(3, 3)
+"""gaussianFilter = Gaussian(3, 3)
 image = np.ones((100, 100))
-print(gaussianFilter.apply_to_image(image))
+print(gaussianFilter.apply_to_image(image))"""
+
+medianFilter = Median(3, 3)
+image = np.ones((100, 100))
+print(medianFilter.apply_to_image(image))
