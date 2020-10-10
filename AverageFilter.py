2cd6f7ea79dbd2c637023c9e86a1c5c99336488a
Maurizio <mau.moraca@gmail.com>
2020-10-10 22:11:29 +0200

    fixed convolution in AverageFilter.py added GaussianFilter methods

--- AverageFilter.py
+++ AverageFilter.py
@@ -13,7 +13,6 @@ class Average(Filter):
                 sum = 0
                 for q in range(i - self.get_up_side_frame(), i - self.get_up_side_frame() + self.get_height()):
                     for r in range(j - self.get_left_side_frame(), j - self.get_left_side_frame() + self.get_width()):
-                        print(q,r)
                         sum = sum + image[q, r]
                 output[i, j] = sum * average
         return output
