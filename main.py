140bb0d564bd779818b7baaa00ac7a0df87d205b
Maurizio <mau.moraca@gmail.com>
2020-10-10 22:19:54 +0200

    fixed convolution in AverageFilter.py added GaussianFilter methods

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 from AverageFilter import Average
 from GaussianFilter import Gaussian
 
-averageFilter = Average(3, 3)
-image = np.ones((10, 5))
-print(averageFilter.apply_to_image(image))
+"""averageFilter = Average(3, 3)
+image = np.ones((1024, 1024))
+print(averageFilter.apply_to_image(image))"""
 
-"""gaussianFilter = Gaussian(3, 3)
-image = np.ones((10, 5))
-print(gaussianFilter.apply_to_image(image))"""
+gaussianFilter = Gaussian(3, 3)
+image = np.ones((100, 100))
+print(gaussianFilter.apply_to_image(image))
