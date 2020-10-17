b1e04526a8de071bae3a104e59c64d705bcfd652
Maurizio <mau.moraca@gmail.com>
2020-10-17 16:51:32 +0200

    Added SharpeningFilter.py

--- SharpeningFilter.py
+++ SharpeningFilter.py
@@ -1,9 +1,7 @@
-from DigitalFilter import Filter
 import numpy as np
-import scipy.stats as st
+from DigitalFilter import Filter
 
-class Gaussian(Filter):
-    __standard_deviation = 0
+class Sharpening(Filter):
     __kernel = None
 
     def __init__(self, size=3):
@@ -12,19 +10,27 @@ class Gaussian(Filter):
         self.make_kernel()
 
     def make_kernel(self):
-        """Returns a 2D Gaussian kernel."""
-        self.set_standard_deviation()
-        x = np.linspace(-self.__standard_deviation, self.__standard_deviation, self.get_height() + 1)
-        kern1d = np.diff(st.norm.cdf(x))
-        kern2d = np.outer(kern1d, kern1d)
-        self.__kernel = kern2d / kern2d.sum()
-        self.__kernel = self.__kernel / self.__kernel[0, 0]
-        self.__kernel = self.__kernel / self.__kernel.sum()
+        self.__kernel = np.zeros((self.get_height(), self.get_width()))
+        row_of_middle_pixel_inside_kernel = int(self.get_height() / 2)
+        column_of_middle_pixel_inside_kernel = int(self.get_width() / 2)
 
-    def set_standard_deviation(self):
-        self.__standard_deviation = (self.get_width() + 1) / 5
+        self.__kernel[row_of_middle_pixel_inside_kernel, column_of_middle_pixel_inside_kernel] = -4
+        for i in range(self.get_height()):
+            if i != row_of_middle_pixel_inside_kernel:
+                self.__kernel[i, column_of_middle_pixel_inside_kernel] = 1
+        for j in range(self.get_width()):
+            if j != column_of_middle_pixel_inside_kernel:
+                self.__kernel[row_of_middle_pixel_inside_kernel, j] = 1
 
     def make_convolution(self, image, output):
+        first_derivative = np.copy(output)
+        second_derivative = np.copy(output)
+        first_derivative = self.apply_derivative(image, first_derivative)
+        second_derivative = self.apply_derivative(first_derivative, second_derivative)
+        output = image + first_derivative - second_derivative
+        return output
+
+    def apply_derivative(self, image, output):
         starting_row = int(self.get_height() / 2)
         starting_column = int(self.get_width() / 2)
         ending_row = image[:, 0].size - self.get_down_side_frame()
@@ -39,6 +45,3 @@ class Gaussian(Filter):
                         convolution = convolution + (self.__kernel[row_kernel, column_kernel] * image[q, r])
                 output[i, j] = int(convolution)
         return output
-
-
-
